5c9eb82ad810f8ee921b00eb7c8bcf0da9096610
thallesgadelha <thallesgadelha10@gmail.com>
2024-07-01 22:22:56 -0300

    teste estagiario atualizado

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const form = document.querySelector('#form')
 const body = document.querySelector('body')
-
 const nome = document.querySelector('#nome')
 const email = document.querySelector('#email')
 const telefone = document.querySelector('#telefone')
+const button = document.querySelector('#button')
+ 
+const handleTelefone = (event) => {
+    let input = event.target
+    input.value = mascaraTelefone(input.value)
+}
 
-const button = document.querySelector('#enviar')
+const mascaraTelefone = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+}
 
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
+    enviar()
+});
+
+function enviar() {
     var dados = {
         Nome: nome.value,
         Email: email.value,
         Telefone: telefone.value
     }
-    
-    form.submit()
-    enviado()
 
-    function enviado(){
-        body.innerHTML = "<h3>Obrigado por seu cadastro!</h3>"
-        console.log(dados)
+    if (nome.value == "" || nome.value == null) {
+        alert("Informe um nome");
+        return;
     }
-    
-    
-});
+
+    if (email.value == "" || email.value == null) {
+        alert("Informe um email");
+        return;
+    }
+
+    if (telefone.value == "" || telefone.value == null) {
+        alert("Informe um telefone");
+        return;
+    }
+
+    body.innerHTML = "<h3>Obrigado por seu cadastro!</h3>"
+    console.log(dados)
+}
